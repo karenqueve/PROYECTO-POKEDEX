ea4e2d52bfc988c2e1d3857f5c6934700dea979b
karen-yquevedo <kyamilequevedo@gmail.com>
2022-06-10 23:25:15 +0200

    add modify html, js & new css

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-let ALL_RESULTS = [];
+let pokemonsData = [];
 
-const getOnePokemonFromApi = async (id) => {
+const getPokemonById = async (id) => {
+  // con esta función pedimos los datos de un pokemon a la api
+  // pasándole un id (número)
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
-  const pokemon = await res.json();
-
-  ALL_RESULTS.push(pokemon);
+  const pokemonData = await res.json();
+  // y al final devolvemos los datos del pokemon con ese id
+  return pokemonData;
 };
 
-const getAllPokemons = async () => {
+const getAllPokemonsData = async () => {
+  // con esta función recorremos 151 números
   for (let id = 1; id <= 151; id++) {
-    await getOnePokemonFromApi(id);
+    // pasándole los 151 números a getPokemonById pedimos
+    // los datos de 151 pokemons y los vamos metiendo en el array
+    const pokemonData = await getPokemonById(id);
+    pokemonsData.push(pokemonData)
   }
-  console.log(ALL_RESULTS);
-  paintPokemons(ALL_RESULTS);
+  paintPokemons(pokemonsData);
 };
-getAllPokemons();
 
 const detalle = (name) => {
   console.log(name);
@@ -23,13 +27,16 @@ const detalle = (name) => {
 
 const paintPokemons = (pokemonsToPaint) => {
 
-  const pokedex$$ = document.querySelector("#pokedex");
-  pokedex$$.innerHTML = "";
+  const pokedex = document.querySelector("#pokedex");
+  pokedex.innerHTML = '';
 
   pokemonsToPaint.forEach((pokemon) => {
 
     const pokemonContainer$$ = document.createElement("div");
     pokemonContainer$$.setAttribute('class', 'cajapokemon');
+
+    pokemonContainer$$.addEventListener('click', () => detalle(pokemon.name));
+
 
     let htmltypes = `<div class= "cajatipos">
     `
@@ -41,31 +48,43 @@ const paintPokemons = (pokemonsToPaint) => {
     htmltypes += `</div>`
 
     const html = `
-            <div>${pokemon.name}<div>
+            <div class="contentCard"><h2>${pokemon.name}</h2><div>
             <img src=${pokemon.sprites.front_default} alt=${pokemon.name}>
-            <div><p>Altura: ${pokemon.height}</p>
+            <div><p>ALTURA: ${pokemon.height}</p>
             <p>${htmltypes}</p></div>
         `;
 
-    pokemonContainer$$.innerHTML = html;
-    pokedex$$.appendChild(pokemonContainer$$)
 
-    const species = document.getElementById('types')
-    types.addEventListener('click', () => detalle(pokemon.name));
+    pokemonContainer$$.innerHTML = html;
+    pokedex.appendChild(pokemonContainer$$);
 
   });
 };
 
+getAllPokemonsData();
 
 function filterByName() {
-  const toFilter = document.getElementById('searchPokedex').value;
+  const toFilter = document.getElementById('searchPokedex').value.toLowerCase().trim();
 
-  const filteredPokemon = ALL_RESULTS.filter(pokemon => pokemon.name.startsWith(toFilter));
+  const filteredPokemon = pokemonsData.filter(pokemon => pokemon.name.startsWith(toFilter));
 
   paintPokemons(filteredPokemon);
-  console.log(filterByName);
-
+  console.log(filteredPokemon);
 };
 
+/*const filterTypes = pokemonsData.filter((pokemon) {
+  const markTypes = null;
+  const markTypesTwo = null;
+
+  if (pokemon.type[1] === "types") {
+    markTypes = pokemon.type[1].name === type;
+  };
+
+  if (pokemon.type[0] === "types") {
+    markTypesTwo = pokemon.type[0].name === type;
+  };
+
+  return markTypes || markTypesTwo;
+});*/
 
 
